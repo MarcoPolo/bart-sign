1a120ea2822611229a1eafc05e04ddbdd66021cf
Marco Munizaga <marco@marcopolo.io>
2019-09-17 21:33:13 -0700

    Got bart data and parsed it

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App: React.FC = () => {
+  // hi!!!
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +11,72 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-}
+};
+
+// http://api.bart.gov/api/etd.aspx?cmd=etd&dir=n&orig=WOAK&key=MW9S-E7SL-26DU-VV8V&json=y
+
+console.log("hello from the console!!!");
+
+const myAdder = (a: number, b: number) => a + b;
+console.log("My adder", myAdder(1, 2));
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+let bartPromise: Promise<Response> = fetch(
+  "http://api.bart.gov/api/etd.aspx?cmd=etd&dir=s&orig=WOAK&key=MW9S-E7SL-26DU-VV8V&json=y"
+);
+console.log(bartPromise);
+
+type ETD = {
+  length: string;
+  minutes: string;
+};
+
+type BartLineETD = {
+  abbreviation: string;
+  destination: string;
+  estimate: Array<ETD>;
+};
+
+type BartStationETD = {
+  root: {
+    station: Array<{
+      abbr: string;
+      etd: Array<BartLineETD>;
+    }>;
+  };
+};
+
+bartPromise
+  .then((resp: Response) => {
+    if (resp.ok) {
+      // https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream
+      console.log("I have the response!");
+      return resp.json();
+    } else {
+      throw new Error("I didn't get the info from bart :(");
+    }
+  })
+  .then((bartStationETD: BartStationETD) => {
+    const etdsForStation = bartStationETD.root.station[0].etd;
+    const allEstimatesForEveryTrain: Array<number> = etdsForStation
+      .map(train => train.estimate.map(etd => etd.minutes))
+      .flat()
+      .map(strMinutes => (strMinutes === "Leaving" ? 0 : parseInt(strMinutes)))
+      .sort();
+
+    console.log("All estimates: ", allEstimatesForEveryTrain);
+  });
+
+// Overview
+// 1. Get the data ✅
+// 2. parse the data/format the data ✅
+// ---
+// 3. Show it off!
+// 4. Allow out url pararmeters
+
+// https://bart.marcopolo.io/
 
 export default App;
